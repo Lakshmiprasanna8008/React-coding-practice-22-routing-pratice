0b931c941a3843fabcd4b293b3aa6843a6c73fbe
LakshmiPrasanna8008 <kondalakshmiprasannareddy@gmail.com>
2024-04-18 18:34:33 +0000

    solution

---
+++ src/components/Header/index.js
@@ -0,0 +1,35 @@
+// Write your JS code here
+import {Link} from 'react-router-dom'
+import './index.css'
+
+// Write your JS code here
+const Header = () => (
+  <div className="whole-header-container">
+    <div className="logo-container">
+      <img
+        className="logo"
+        src="https://assets.ccbp.in/frontend/react-js/wave-logo-img.png"
+        alt="wave"
+      />
+      <h1>Wave</h1>
+    </div>
+    <ul className="nav-menu">
+      <li>
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link className="nav-link" to="/about">
+          About
+        </Link>
+      </li>
+      <li>
+        <Link className="nav-link" to="/contact">
+          Contact
+        </Link>
+      </li>
+    </ul>
+  </div>
+)
+export default Header
